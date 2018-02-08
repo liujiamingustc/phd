a579c2d241ecfc520eb00b32c01d0972c8bf26fa
quanpan <quanpan302@hotmail.com>
2018-02-08 18:30:10 +0100

    Create src

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+# Author: Quan Pan <quanpan302@hotmail.com>
+# License: Apache 2.0 License
+# Create: 2016-12-02
+
+"""Examples
+Links:
+    http://scipy.org
+    SciPy: http://scipy.github.io/devdocs/
+    NumPy: https://docs.scipy.org/doc/numpy-dev/reference/
+
+"""
+
+
+__author__ = "Quan Pan"
+__version__ = "1.0"
+__revision__ = "1.0.0"
