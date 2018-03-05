53921e38646f1e62c51ee0637fd62d85df98280a
quanpan <quanpan302@hotmail.com>
2018-03-05 19:37:26 +0100

    Create mongodb

---
+++ docker/microservices/mongodb/example/DOCKER_DB_BIG-mongo-master/generate-stackbrew-library.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+set -eu
+
+declare -A aliases=(
+	[3.6]='3 latest'
+	[3.7]='unstable'
+	[3.8-rc]='rc'
+)
+
+self="$(basename "$BASH_SOURCE")"
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( */ )
+versions=( "${versions[@]%/}" )
+
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		fileCommit \
+			Dockerfile \
+			$(git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++) {
+						print $i
+					}
+				}
+			')
+	)
+}
+
+cat <<-EOH
+# this file is generated via https://github.com/docker-library/mongo/blob/$(fileCommit "$self")/$self
+
+Maintainers: Tianon Gravi <admwiggin@gmail.com> (@tianon),
+             Joseph Ferguson <yosifkit@gmail.com> (@yosifkit)
+GitRepo: https://github.com/docker-library/mongo.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
+for version in "${versions[@]}"; do
+	rcVersion="${version%-rc}"
+
+	commit="$(dirCommit "$version")"
+
+	fullVersion="$(git show "$commit":"$version/Dockerfile" | awk '$1 == "ENV" && $2 == "MONGO_VERSION" { gsub(/~/, "-", $3); print $3; exit }')"
+
+	versionAliases=()
+	if [ "$version" = "$rcVersion" ]; then
+		while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
+			versionAliases+=( $fullVersion )
+			fullVersion="${fullVersion%[.-]*}"
+		done
+	else
+		versionAliases+=( $fullVersion )
+	fi
+	versionAliases+=(
+		$version
+		${aliases[$version]:-}
+	)
+
+	variant="$(git show "$commit":"$version/Dockerfile" | awk '$1 == "FROM" { gsub(/^.*:|-.*$/, "", $2); print $2; exit }')"
+
+	variantAliases=( "${versionAliases[@]/%/-$variant}" )
+	variantAliases=( "${variantAliases[@]//latest-/}" )
+
+	echo
+	cat <<-EOE
+		Tags: $(join ', ' "${variantAliases[@]}")
+		SharedTags: $(join ', ' "${versionAliases[@]}")
+		# see http://repo.mongodb.org/apt/debian/dists/$variant/mongodb-org/${version}/main/
+		# (i386 is empty, as is ppc64el)
+		Architectures: amd64
+		GitCommit: $commit
+		Directory: $version
+	EOE
+
+	for v in \
+		windows/windowsservercore-{ltsc2016,1709} \
+		windows/nanoserver-{sac2016,1709} \
+	; do
+		# we run into https://github.com/moby/moby/issues/32838 fairly consistently
+		# as such, Windows builds are temporarily disabled
+		continue
+
+		dir="$version/$v"
+		variant="$(basename "$v")"
+
+		[ -f "$dir/Dockerfile" ] || continue
+
+		commit="$(dirCommit "$dir")"
+
+		variantAliases=( "${versionAliases[@]/%/-$variant}" )
+		variantAliases=( "${variantAliases[@]//latest-/}" )
+
+		sharedTags=()
+		for windowsShared in windowsservercore nanoserver; do
+			if [[ "$variant" == "$windowsShared"* ]]; then
+				sharedTags=( "${versionAliases[@]/%/-$windowsShared}" )
+				sharedTags=( "${sharedTags[@]//latest-/}" )
+				break
+			fi
+		done
+		if [[ "$variant" == 'windowsservercore'* ]]; then
+			sharedTags+=( "${versionAliases[@]}" )
+		fi
+
+		echo
+		echo "Tags: $(join ', ' "${variantAliases[@]}")"
+		if [ "${#sharedTags[@]}" -gt 0 ]; then
+			echo "SharedTags: $(join ', ' "${sharedTags[@]}")"
+		fi
+		cat <<-EOE
+			Architectures: windows-amd64
+			GitCommit: $commit
+			Directory: $dir
+		EOE
+		[ "$variant" = "$v" ] || echo "Constraints: $variant"
+	done
+done
---
+++ docker/microservices/mongodb/example/DOCKER_DB_BIG-mongo-master/update.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+# TODO do something with https://www.mongodb.org/dl/linux/x86_64 instead of scraping the APT repo contents
+# (but then have to solve hard "release candidate" problems; ie, if we have 2.6.4 and 2.6.5-rc0 comes out, we don't want 2.6 to switch over to the RC)
+
+travisEnv=
+appveyorEnv=
+for version in "${versions[@]}"; do
+	rcVersion="${version%-rc}"
+	major="$rcVersion"
+	rcGrepV='-v'
+	if [ "$rcVersion" != "$version" ]; then
+		rcGrepV=
+		major='testing'
+	fi
+
+	from="$(gawk -F '[[:space:]]+' 'toupper($1) == "FROM" { print $2; exit }' "$version/Dockerfile")" # "debian:xxx"
+	suite="${from#*:}" # "wheezy-slim" or "jessie-slim"
+	suite="${suite%-slim}" # "wheezy" or "jessie"
+
+	packagesUrl="https://repo.mongodb.org/apt/debian/dists/$suite/mongodb-org/$major/main/binary-amd64/Packages"
+	fullVersion="$(
+		curl -fsSL "$packagesUrl.gz" \
+			| gunzip \
+			| awk -F ': ' '
+				$1 == "Package" { pkg = $2 }
+				pkg ~ /^mongodb-(org(-unstable)?|10gen)$/ && $1 == "Version" { print $2 "=" pkg }
+			' \
+			| grep "^$rcVersion\." \
+			| grep -v '~pre~$' \
+			| sort -V \
+			| tail -1
+	)"
+	packageName="${fullVersion#*=}"
+	fullVersion="${fullVersion%=$packageName}"
+
+	(
+		set -x
+		sed -ri \
+			-e 's/^(ENV MONGO_MAJOR) .*/\1 '"$major"'/' \
+			-e 's/^(ENV MONGO_VERSION) .*/\1 '"$fullVersion"'/' \
+			-e 's/^(ARG MONGO_PACKAGE)=.*/\1='"$packageName"'/' \
+			"$version/Dockerfile"
+	)
+
+	if [ -d "$version/windows" ]; then
+		windowsUrlPrefix='http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-'
+		windowsUrlSuffix='-signed.msi'
+		windowsVersions="$(
+			curl -fsSL 'https://www.mongodb.org/dl/win32/x86_64-2008plus-ssl' \
+				| grep --extended-regexp --only-matching '"'"${windowsUrlPrefix}${rcVersion//./\\.}"'\.[^"]+'"${windowsUrlSuffix}"'"' \
+				| sed \
+					-e 's!^"'"$windowsUrlPrefix"'!!' \
+					-e 's!'"$windowsUrlSuffix"'"$!!' \
+				| grep $rcGrepV -- '-rc'
+		)"
+		windowsLatest="$(echo "$windowsVersions" | head -1)"
+		windowsSha256="$(curl -fsSL "${windowsUrlPrefix}${windowsLatest}${windowsUrlSuffix}.sha256" | cut -d' ' -f1)"
+
+		(
+			set -x
+			sed -ri \
+				-e 's/^(ENV MONGO_VERSION) .*/\1 '"$windowsLatest"'/' \
+				-e 's/^(ENV MONGO_DOWNLOAD_SHA256) .*/\1 '"$windowsSha256"'/' \
+				"$version/windows/"*"/Dockerfile"
+		)
+
+		for winVariant in \
+			nanoserver-{1709,sac2016} \
+			windowsservercore-{1709,ltsc2016} \
+		; do
+			[ -f "$version/windows/$winVariant/Dockerfile" ] || continue
+
+			sed -ri \
+				-e 's!^FROM .*!FROM microsoft/'"${winVariant%%-*}"':'"${winVariant#*-}"'!' \
+				"$version/windows/$winVariant/Dockerfile"
+
+			case "$winVariant" in
+				*-1709) ;; # no AppVeyor support for 1709 yet: https://github.com/appveyor/ci/issues/1885
+				*) appveyorEnv='\n    - version: '"$version"'\n      variant: '"$winVariant$appveyorEnv" ;;
+			esac
+		done
+	fi
+
+	travisEnv='\n  - VERSION='"$version$travisEnv"
+done
+
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
+
+appveyor="$(awk -v 'RS=\n\n' '$1 == "environment:" { $0 = "environment:\n  matrix:'"$appveyorEnv"'" } { printf "%s%s", $0, RS }' .appveyor.yml)"
+echo "$appveyor" > .appveyor.yml
